d4e5a2037215a943420138e0d3a1e17944470f04
Supriyo Ain <100848060+rikisupriyo@users.noreply.github.com>
2022-05-01 00:46:31 +0530

    Add files via upload

---
+++ main1.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        bool isEven(int n){
+            if(n%2 == 0)
+                return true;
+            return false;
+        }
+
+        vector<double> merge(vector<int> &num1, vector<int> &num2){
+            int i=0, j=0;
+            vector<double> num3;
+            while(i<num1.size() && j<num2.size()){
+                if(num1[i] < num2[j])
+                    num3.push_back(num1[i++]);
+                else
+                    num3.push_back(num2[j++]);
+            }
+            while(i<num1.size())
+                num3.push_back(num1[i++]);
+            while(j<num2.size())
+                num3.push_back(num2[j++]);
+
+            return num3;
+        }
+
+        double findMedianSortedArrays(vector<int> &num1, vector<int> &num2){
+            vector<double> num3 = merge(num1, num2);
+            int n = num3.size();
+            
+            if(isEven(n))
+                return  (num3[n/2]+num3[(n/2)-1])/2;
+            return num3[n/2];
+        }
+};
+
+int main(){
+    Solution s;
+    vector<int> num1, num2;
+    num1.push_back(1);
+    num1.push_back(2);
+    num2.push_back(3);
+    num2.push_back(4);
+    cout<<s.findMedianSortedArrays(num1, num2);
+}

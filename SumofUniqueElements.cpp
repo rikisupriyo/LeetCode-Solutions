ea2034de6bf9cab1b25d10424dbe54a1818281f2
Supriyo Ain <100848060+rikisupriyo@users.noreply.github.com>
2023-04-08 15:07:01 +0530

    Create SumofUniqueElements.cpp

---
+++ SumofUniqueElements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        unordered_map<int, int> m;
+
+        for (auto i:nums) 
+            m[i]++;
+
+        int s=0;
+        for (auto i:m) {
+            if (i.second == 1)
+                s += i.first;
+        }
+        return s;
+    }
+};
